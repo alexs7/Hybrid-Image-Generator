0315d1f970a02925cb91505e1c4742bf41f388cc
alex <alexandrosrotsides@gmail.com>
2016-02-23 23:01:25 +0000

    simple gaussian template

--- src/com/alexs7/ImageProcessor.java
+++ src/com/alexs7/ImageProcessor.java
@@ -7,13 +7,33 @@ import javafx.scene.image.Image;
  */
 public class ImageProcessor {
 
-    public Image applyGaussianFilter(Image image){
-
-        return image;
-    }
-
     public void generateHybridImage(Image firstImage, Image secondImage, int templateWidthInt, int templateHeightInt) {
 
+        getGaussianTemplate(templateWidthInt,templateHeightInt, 0.1);
 
+    }
+
+    public double[][] getGaussianTemplate(int width, int height, double sigma){
+        double[][] gaussianTemplate = new double [height][width];
+        double coefficient = 0.0;
+        double sum = 0.0;
+
+        for (int x = 0; x < width; x++) {
+            for (int y = 0; y < height; y++) {
+                coefficient = Math.exp(-((x*x+y*y)/2*sigma*sigma))/(2*Math.PI*sigma*sigma); //??
+                sum = sum + coefficient;
+                gaussianTemplate[y][x] = coefficient;
+            }
+        }
+
+        for (int x = 0; x < width; x++) {
+            for (int y = 0; y < height; y++) {
+                gaussianTemplate[y][x] = gaussianTemplate[y][x] / sum;
+                System.out.print(" " + gaussianTemplate[y][x]);
+            }
+            System.out.println();
+        }
+
+        return gaussianTemplate;
     }
 }
